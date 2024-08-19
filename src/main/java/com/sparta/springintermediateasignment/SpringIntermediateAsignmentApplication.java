608b06b224ec395c2c184d3b06eef6e72fb8137c
Nameless1004 <jjho1029@gmail.com>
2024-08-19 12:23:05 +0900

    init project

---
+++ src/main/java/com/sparta/springintermediateasignment/SpringIntermediateAsignmentApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.springintermediateasignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIntermediateAsignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringIntermediateAsignmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/springintermediateasignment/SpringIntermediateAsignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.springintermediateasignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIntermediateAsignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
